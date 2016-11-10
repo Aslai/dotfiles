02ce57cdc3a7e8a97dd81ad717fb9cdaaf9aaccc
Andrew Story <kaslai@kaslai.com>
2016-11-10 02:08:20 -0800

    Update

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Just a centralized repo of my configs.
 
+Consider adding:
+* deluge
+* clementine
+
--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,38 @@
 # Shamelessly lifted from http://stackoverflow.com/a/246128/670358 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+function prep {
+	rm -r $1
+	mkdir -p $(dirname "$1")
+}
 function link {
-	rm $1/$2 2>/dev/null
+	prep "$1/$2"
 	ln -s ${DIR}/$2 $1/$2
 }
 function copy {
-        rm $1/$2 2>/dev/null
+        prep "$1/$2"
         cp ${DIR}/$2 $1/$2
+}
+function render {
+        prep "$1/$2"
+	args_in=("$@");
+	args=();
+	for arg in "${args_in[@]:2}"; do
+		read -e -p "${arg}: ";
+		args=("${args[@]}" "${REPLY}");
+	done
+        ./$2 "${args[@]}" > "$1/$2"
 }
 
 link ~ .SciTEUser.properties
 link ~ .gitconfig
 copy ~/.config/codeblocks cbKeyBinder10.ini
+copy ~/.codeblocks cbKeyBinder10.ini
 
+render ~/.config hexchat/servlist.conf "ZNC Password" "ZNC Port"
+link ~/.config hexchat/colors.conf
+link ~/.config hexchat/ctcpreply.conf
+link ~/.config hexchat/hexchat.conf
+
+link ~/.config antimicro
+link ~/.config keepassx
